b9e4af08e33343e369fffd6493f8f623aefd5a3c
25LP3402 25LP3402 <boloroots47@gmail.com>
2025-06-17 12:45:02 +0800

    :

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,39 @@
 import express, { Request, Response } from "express";
-import { User } from "./types/types";
+// import fs from "fs-extra";
+// import { User } from "./types/types";
+import userRouter from "./router/user/user";
 
 const app = express();
-const port = 4200;
+const port = 3000;
 app.use(express.json());
 
-app.get("/", (req, res) => {
+
+
+app.use("/", userRouter);
+
+app.get("/", (req:Request, res:Response) => {
   res.send({
     name: "Bolormaa",
     age: "18",
   });
 });
 
-app.post("/user", (req: Request, res: Response) => {
-  console.log("post req", req.body);
-
-  const { name, age } = req.body;
-  //   res.send("Successfully created user")
-  res.json({ message: `User ${name} is ${age}` });
-});
-
-app.put("/updateUser", (req: Request, res: Response) => {
-  const { name, age }: { name: string; age: string } = req.body;
-  res.send(`updated user ${name} ${age}`);
-});
 
 //Create user
-app.post("/creatUser", (req: Request, res: Response) => {
-  const { name, age, userName, userEmail, phoneNumber, password }: User =
-    req.body;
-});
+
+
+// app.get("/users", (req:Request, res:Response)=>{
+
+  
+//   const users = fs.readFileSync("./user.json", {encoding: "utf8", flag:"r"});
+//   console.log("users", users);
+  
+//   res.json(JSON.parse(users));
+// })
+
+// app.delete("/delete", (req:Request, res:Response)=>{})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
-
-// app.listen(3000, ()=>{
-//     console.log(`Example app listening on port http://localhost:3000`);
-
-// })
---
+++ src/router/user/user.ts
@@ -0,0 +1,81 @@
+import express, { Request, Response } from "express";
+import fs, { existsSync } from "fs-extra";
+import { User } from "../../types/types";
+
+const userRouter = express.Router();
+
+userRouter.post("/user", (req: Request, res: Response) => {
+  console.log("post req", req.body);
+  const { name, age } = req.body;
+  // res.send("Successfully created useRR")
+  res.json({ message: `User ${name} is ${age}` });
+});
+
+userRouter.get("/users", (req: Request, res: Response) => {
+  const users = fs.readFileSync("./user.json", { encoding: "utf8", flag: "r" });
+  console.log("users", users);
+
+  res.json(JSON.parse(users));
+});
+
+userRouter.post("/createUser", (req: Request, res: Response) => {
+  const { name, age, userName, userEmail, phoneNumber, password }: User =
+    req.body;
+
+  const uniqueId = Math.random();
+
+  const filePath = "./user.json";
+
+  let users: User[] = [];
+
+  if (fs.existsSync(filePath)) {
+    const existingData = fs.readFileSync(filePath, "utf8");
+    console.log("existing data:", existingData);
+
+    if (existingData.trim().length > 0) {
+      users = JSON.parse(existingData);
+    }
+  }
+  users.push({
+    name,
+    age,
+    userName,
+    userId: uniqueId,
+    userEmail,
+    phoneNumber,
+    password,
+  });
+
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+
+  res.send("Amjjilttai burtgegdew");
+});
+
+
+userRouter.delete("/deleteUser", (req: Request, res: Response) => {
+    const {userId} :{userId:number}= req.body;
+    console.log("ID", userId);
+    
+  const users = fs.readFileSync("./user.json", "utf8");
+
+  console.log(  "DELETED ID USERS", users); //ID-GUIGEER BUSAD NI HEWLEGDEJ BAINA
+
+  const arrayUsers = JSON.parse(users);
+  console.log(typeof arrayUsers);
+
+  
+  const deletedUser = arrayUsers.filter((delete:any)=> delete.userId !== userId);
+console.log(deletedUser);
+
+//   res.json(JSON.parse(users))
+
+//   res.send(`deleted id ${deleteId.userId}`)
+
+});
+
+// userRouter.put("/updateUser", (req: Request, res: Response) => {
+//   const { name, age }: { name: string; age: string } = req.body;
+//   res.send(`updated user ${name} ${age}`);
+// });
+
+export default userRouter;
